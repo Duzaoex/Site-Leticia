07cf62fd918094fad849741d47232d910d7b37c8
Duzaoex <eduardoneriscarneiro@gmail.com>
2024-07-13 16:28:57 -0300

    le

--- src/Pages/SobreMim/SobreMim.tsx
+++ src/Pages/SobreMim/SobreMim.tsx
@@ -17,15 +17,15 @@ function SobreMim() {
                         Minha dedicação e trabalho em ofertar
                         um serviço de saúde mental acontecem porque acredito que o cuidado, o acolhimento, à
                         escuta, as técnicas da teoria da psicanálise podem proporcionar tratamento aos
-                        sofrimentos, queixas e sintomas que causam adoecimento.
+                        sofrimentos, queixas e sintomas que causam adoecimento
                     </p>
                     <p>
                         Atendo adolescentes, jovens e adultos que se encontram em sofrimento e incômodo com
-                        questões que vêm passando.
+                        questões que vêm passando
                     </p>
                     <p>
                         Além da graduação em Psicologia, estou em constante formação através de Grupos de
-                        Estudos, Supervisão e Estudo Teórico que conduzem ao processo de análise.
+                        Estudos, Supervisão e Estudo Teórico que conduzem ao processo de análise
                     </p>
                 </div>
             </div>
